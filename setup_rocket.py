15a8f6d4a007198eb9f49713f83e9776562042e9
Abhiroop Ghosh <abhiroopghosh.71@gmail.com>
2024-09-15 21:21:15 -0500

    Using setuptools instead of distools. Added python 3.10 checking to GitHub actions yml

--- setup_rocket.py
+++ setup_rocket.py
@@ -1,5 +1,5 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 
 import numpy as np
 from Cython.Build import cythonize
