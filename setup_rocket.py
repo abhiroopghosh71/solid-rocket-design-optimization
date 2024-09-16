a583acc293c346bbcce4f6bfc6b116a8029f70b3
Abhiroop Ghosh <abhiroopghosh.71@gmail.com>
2024-09-15 21:27:45 -0500

    Remove defunct -fopenmp option

--- setup_rocket.py
+++ setup_rocket.py
@@ -9,12 +9,9 @@ approcket_extension = Extension(
     sources=["approcket.pyx"],
     libraries=["approcket"],
     library_dirs=["librocket"],
-    include_dirs=["librocket", np.get_include()],
-    extra_compile_args=['-fopenmp'],
-    extra_link_args=['-fopenmp']
+    include_dirs=["librocket", np.get_include()]
 )
 setup(
     name="approcket",
-    ext_modules=cythonize([approcket_extension], gdb_debug=True, compiler_directives={'language_level': "3"}),
-    # version=__version__
+    ext_modules=cythonize([approcket_extension], gdb_debug=True, compiler_directives={'language_level': "3"})
 )
